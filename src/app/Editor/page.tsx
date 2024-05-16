5a6dd1b945b3c39dc278f99cf27d086607c1480d
LAVINIABENZAR <loredanabenzar2@gmail.com>
2024-05-16 14:31:05 +0300

    build: handleChange function

--- src/app/Editor/page.tsx
+++ src/app/Editor/page.tsx
@@ -29,26 +29,33 @@ function Editor() {
         ingredients: [],
         instructions: []
     });
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target
+        setArticle(prevArticle => ({
+            ...prevArticle,
+            [name]: value
+        }))
+    }
     
-   
     return (
         <>
             <form className="bg-pink-100 p-4" onSubmit={handleSubmit}>
                 <div className="flex mb-4 justify-center">
-                     <ImagesInputs/>
+                    <ImagesInputs />
                 </div>
                 <div className='flex flex-col'>
-                <div className="flex mb-4 justify-center">
-                    <label className="block mb-2 mr-3 font-semibold text-lg" htmlFor="title">Title</label>
-                    <input className=" w-90" type="text" id='title' name='title' value={article.title} onChange={handleChange}/>
-                </div> 
-                <div className='text-center'>from</div>
-                <div className="flex mt-3 mb-4 justify-center">
-                    <label className="block mb-2 mr-3 font-semibold text-lg" htmlFor="movie"> Movie / Book Name</label>
-                    <input  type="text" id='movie' name='movie' value={article.movie} onChange={handleChange} />
+                    <div className="flex mb-4 justify-center">
+                        <label className="block mb-2 mr-3 font-semibold text-lg" htmlFor="title">Title</label>
+                        <input className=" w-90" type="text" id='title' name='title' value={article.title} onChange={handleChange} />
+                    </div>
+                    <div className='text-center'>from</div>
+                    <div className="flex mt-3 mb-4 justify-center">
+                        <label className="block mb-2 mr-3 font-semibold text-lg" htmlFor="movie"> Movie / Book Name</label>
+                        <input type="text" id='movie' name='movie' value={article.movie} onChange={handleChange} />
+                    </div>
                 </div>
-                </div>
-                
+
                 <div className="flex flex-col mb-4 place-items-center">
                     <label className="block mb-2 mr-8 text-center font-medium mt-10 text-2xl text-gray-800" htmlFor="ingredients">INGREDIENTS :</label>
                     <input itemID='ingredient-input' className='w-2/6' type="text" id='ingredients' name='ingredients' value={article.ingredients} onChange={handleChange} />
@@ -56,11 +63,11 @@ function Editor() {
                 </div>
                 <div className="flex flex-col mb-4 place-items-center">
                     <label className="block mb-2 mr-8 text-center mt-10 font-medium text-gray-800 text-2xl" htmlFor="text">INSTRUCTIONS :</label>
-                    <input itemID='indtruction-input' className=' w-2/6'  type="text" id='instructions' name='instructions' value={article.instructions} onChange={handleChange}/>
+                    <input itemID='indtruction-input' className=' w-2/6' type="text" id='instructions' name='instructions' value={article.instructions} onChange={handleChange} />
                     <button type='button' onClick={AddInstruction} className="bg-purple-300 w-40 text-white hover:bg-purple-200 font-bold py-2 px-4 mt-3 rounded items-center">Add Instruction</button>
                 </div>
-            <div className='flex justify-end m-9 bg-pink-100'>
-            <button type='submit' className='bg-gray-500 w-1/5 text-white hover:bg-purple-600 font-bold py-2 px-4 mt-3 border-white rounded-xl'>Post</button>
+                <div className='flex justify-end m-9 bg-pink-100'>
+                    <button type='submit' className='bg-gray-500 w-1/5 text-white hover:bg-purple-600 font-bold py-2 px-4 mt-3 border-white rounded-xl'>Post</button>
                 </div>
             </form>
         </>
