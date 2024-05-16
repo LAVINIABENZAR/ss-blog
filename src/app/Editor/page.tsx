34deae285488d4c74d27aa22403e93a33ed1f026
LAVINIABENZAR <loredanabenzar2@gmail.com>
2024-05-16 14:29:26 +0300

    build: define articles interface

--- src/app/Editor/page.tsx
+++ src/app/Editor/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import 'tailwindcss/tailwind.css';
+import { useState } from 'react';
 import ImagesInputs from '@/components/imagesInputs';
 import { SupabaseClient, createClient } from '@supabase/supabase-js';
 
@@ -7,37 +8,59 @@ const URL = process.env.NEXT_PUBLIC_URL || ''
 const API = process.env.NEXT_PUBLIC_API || ''
 const supabase: SupabaseClient = createClient(URL, API)
 
+interface Articles {
+    id: number;
+    image1: string;
+    image2: string;
+    title: string;
+    movie: string;
+    ingredients: string[];
+    instructions: string[];
+}
+
+
 function Editor() {
+    const [article, setArticle] = useState<Articles>({
+        id: 0,
+        image1: '',
+        image2: '',
+        title: '',
+        movie: '',
+        ingredients: [],
+        instructions: []
+    });
+    
+   
     return (
         <>
-            <form className="bg-pink-100 p-4">
+            <form className="bg-pink-100 p-4" onSubmit={handleSubmit}>
                 <div className="flex mb-4 justify-center">
                      <ImagesInputs/>
                 </div>
                 <div className='flex flex-col'>
                 <div className="flex mb-4 justify-center">
-                    <label className="block mb-2 mr-3 font-semibold text-lg" htmlFor="text">Title</label>
-                    <input className=" w-90" type="text" />
+                    <label className="block mb-2 mr-3 font-semibold text-lg" htmlFor="title">Title</label>
+                    <input className=" w-90" type="text" id='title' name='title' value={article.title} onChange={handleChange}/>
                 </div> 
                 <div className='text-center'>from</div>
                 <div className="flex mt-3 mb-4 justify-center">
-                    <label className="block mb-2 mr-3 font-semibold text-lg" htmlFor="text"> Movie / Book Name</label>
-                    <input  type="text" />
+                    <label className="block mb-2 mr-3 font-semibold text-lg" htmlFor="movie"> Movie / Book Name</label>
+                    <input  type="text" id='movie' name='movie' value={article.movie} onChange={handleChange} />
                 </div>
                 </div>
                 
                 <div className="flex flex-col mb-4 place-items-center">
-                    <label className="block mb-2 mr-8 text-center font-medium mt-10 text-2xl text-gray-800" htmlFor="text">INGREDIENTS :</label>
-                    <input className='w-2/6' type="text" />
-                    <button className="bg-purple-300 w-40 text-white hover:bg-purple-200 font-bold py-2 px-4 mt-3 rounded items-center">Add Ingredient</button>
+                    <label className="block mb-2 mr-8 text-center font-medium mt-10 text-2xl text-gray-800" htmlFor="ingredients">INGREDIENTS :</label>
+                    <input itemID='ingredient-input' className='w-2/6' type="text" id='ingredients' name='ingredients' value={article.ingredients} onChange={handleChange} />
+                    <button type='button' onClick={addIngredient} className="bg-purple-300 w-40 text-white hover:bg-purple-200 font-bold py-2 px-4 mt-3 rounded items-center">Add Ingredient</button>
                 </div>
                 <div className="flex flex-col mb-4 place-items-center">
                     <label className="block mb-2 mr-8 text-center mt-10 font-medium text-gray-800 text-2xl" htmlFor="text">INSTRUCTIONS :</label>
-                    <input className=' w-2/6'  type="text" />
-                    <button className="bg-purple-300 w-40 text-white hover:bg-purple-200 font-bold py-2 px-4 mt-3 rounded items-center">Add Instruction</button>
+                    <input itemID='indtruction-input' className=' w-2/6'  type="text" id='instructions' name='instructions' value={article.instructions} onChange={handleChange}/>
+                    <button type='button' onClick={AddInstruction} className="bg-purple-300 w-40 text-white hover:bg-purple-200 font-bold py-2 px-4 mt-3 rounded items-center">Add Instruction</button>
                 </div>
-                <div className='flex justify-end m-9'>
-                <button className='bg-gray-500 w-1/5 text-white hover:bg-purple-600 font-bold py-2 px-4 mt-3 border-white rounded-xl '>Post</button>
+            <div className='flex justify-end m-9 bg-pink-100'>
+            <button type='submit' className='bg-gray-500 w-1/5 text-white hover:bg-purple-600 font-bold py-2 px-4 mt-3 border-white rounded-xl'>Post</button>
                 </div>
             </form>
         </>
