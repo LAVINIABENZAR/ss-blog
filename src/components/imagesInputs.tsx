ae16797e0b874f69bf6efca9df8ab8da1640af90
LAVINIABENZAR <loredanabenzar2@gmail.com>
2024-05-21 19:02:48 +0300

    refactor: imagesInputs

--- src/components/imagesInputs.tsx
+++ src/components/imagesInputs.tsx
@@ -1,13 +1,13 @@
 import 'tailwindcss/tailwind.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 interface ImagesInputsProps {
-    onImageChange: (imageKey: 'image1' | 'image2', src: string) => void;
-    images: { image1: string; image2: string };
+    onImageChange: (imageKey: string, src: string) => void;
+    images: { [key: string]: string };
 }
 
-function ImagesInputs({ onImageChange, images }: ImagesInputsProps) {
-    const handleFileInput = (event: React.ChangeEvent<HTMLInputElement>, imageKey: 'image1' | 'image2') => {
+const ImagesInputs: React.FC<ImagesInputsProps> = ({ onImageChange, images }) => {
+    const handleFileInput = (event: React.ChangeEvent<HTMLInputElement>, imageKey: string) => {
         const file = event.target.files?.[0];
         if (file && file.type.startsWith('image/')) {
             const reader = new FileReader();
@@ -20,31 +20,24 @@ function ImagesInputs({ onImageChange, images }: ImagesInputsProps) {
     };
 
     return (
-        <>
-            <div className='flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100'>
-                {images.image1 ? (
-                    <img className='w-full h-full object-cover' src={images.image1} alt='image 1' />
-                ) : (
-                    <label htmlFor="file1" className='flex flex-col items-center justify-center w-full h-full cursor-pointer'>
-                        <span>Upload Image 1</span>
-                    </label>
-                )}
-                <input type="file" id='file1' className='hidden' name='file1' onChange={(e) => handleFileInput(e, 'image1')} accept='image/*' />
-            </div>
-            <div className='flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100'>
-                {images.image2 ? (
-                    <img className='w-full h-full object-cover' src={images.image2} alt='image 2' />
-                ) : (
-                    <label htmlFor="file2" className='flex flex-col items-center justify-center w-full h-full cursor-pointer'>
-                        <span>Upload Image 2</span>
-                    </label>
-                )}
-                <input type="file" id='file2' className='hidden' name='file2' onChange={(e) => handleFileInput(e, 'image2')} accept='image/*' />
-            </div>
-        </>
+        <div className="flex flex-wrap gap-4">
+            {Object.keys(images).map(imageKey => (
+                <div key={imageKey} className='flex flex-col items-center justify-center w-fit h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100'>
+                    {images[imageKey] ? (
+                        <img className='w-full h-full object-contain' src={images[imageKey]} alt={imageKey} />
+                    ) : (
+                        <label htmlFor={imageKey} className='flex flex-col items-center justify-center w-full h-full cursor-pointer'>
+                            <span>Upload {imageKey}</span>
+                        </label>
+                    )}
+                    <input type="file" id={imageKey} className='hidden' name={imageKey} onChange={(e) => handleFileInput(e, imageKey)} accept='image/*' />
+                </div>
+            ))}
+        </div>
     );
-}
+};
 
 export default ImagesInputs;
+
 
 
