6e93e9294b469f8c3383798e8d80addd149d3aa7
LAVINIABENZAR <loredanabenzar2@gmail.com>
2024-05-21 16:13:37 +0300

    build: articles interface and supabase

---
+++ src/app/Posts/page.tsx
@@ -0,0 +1,27 @@
+'use client'
+import React, { useEffect, useState } from "react";
+import { PostgrestResponse, createClient } from "@supabase/supabase-js";
+
+const URL = process.env.NEXT_PUBLIC_URL || ''
+const API = process.env.NEXT_PUBLIC_API || ''
+const supabase = createClient(URL, API)
+
+interface Posts {
+    id: number,
+    image1: string;
+    image2: string;
+    title: string;
+    movie: string;
+    ingredients: string[];
+    instructions: string[];
+    description: string
+}
+
+export default function Posts() {
+    const [posts, setPosts] = useState<Posts[]>([])
+
+    return(
+        <>
+        </>
+    )
+}
