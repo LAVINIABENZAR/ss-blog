96c5fc8d1629cdde4865c8db9fd5914645f5b0ef
LAVINIABENZAR <loredanabenzar2@gmail.com>
2024-06-03 22:33:48 +0300

    feat: error handling on supabase connection strings

--- src/utils/supabase.tsx
+++ src/utils/supabase.tsx
@@ -3,4 +3,10 @@ import { createClient } from '@supabase/supabase-js';
 const URL = process.env.NEXT_PUBLIC_URL!;
 const API = process.env.NEXT_PUBLIC_API!;
 
+if (!URL || !API) {
+    throw new Error('Missing Supabase URL or Key');
+  }
+
 export const supabase = createClient(URL, API);
+
+
