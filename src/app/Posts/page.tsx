fe2d7e7816d19e819f377026f88093f13922faeb
LAVINIABENZAR <loredanabenzar2@gmail.com>
2024-06-15 13:33:42 +0300

    build: fetchPosts function

--- src/app/Posts/page.tsx
+++ src/app/Posts/page.tsx
@@ -1,65 +1,26 @@
 'use client'
-import React, { useEffect, useState } from "react";
-import { PostgrestResponse } from "@supabase/supabase-js";
-import { supabase } from "@/utils/supabase";
+import React from "react";
+import { getPosts } from "@/utils/getPosts";
+import { useState, useEffect } from "react";
+import { Post } from "@/utils/postInterface";
 
-interface Posts {
-    id: number,
-    image1: string;
-    image2: string;
-    title: string;
-    movie: string;
-    ingredients: string[];
-    instructions: { text: string, image: string }[];
-    description: string
-}
+const Posts = () => {
+    const [posts, setPosts] = useState<Post[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
 
-export default function Posts() {
-    const [posts, setPosts] = useState<Posts[]>([])
+  
+    const fetchPosts = async () => {
+      try {
+        const data = await getPosts();
+        setPosts(data);
+      } catch (error: unknown) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    useEffect(() => {
-        getPosts()
-    }, [])
 
-    async function getPosts() {
-        const {data}: PostgrestResponse<Posts> = await supabase
-         .from('articles')
-         .select()
-         setPosts(data || [])
-    } 
-
-    return(
-        <>
-        {posts.map((post) => (
-            <div>
-                <h2>
-                    {post.title}
-                </h2>
-                <span>from</span>
-                <h3>
-                    {post.movie}
-                </h3>
-                <section>
-                    {post.description}
-                </section>
-                <img src={post.image1} alt="image" width={200}/>
-                <h3>Ingredients:</h3>
-                <ul>
-                  {post.ingredients.map((ingredient) => (
-                    <li>{ingredient}</li>
-                  ))}
-                </ul>
-                <h3>Instructions</h3>
-                <ul>
-                    {post.instructions.map((instruction) => (
-                        <>
-                        <li>{instruction.text}</li>
-                        <img src={instruction.image} width={200}  />
-                        </>
-                    ))}
-                </ul>
-            </div>
-        ))}
-        </>
-    )
-}
+  };
+  
+  export default Posts;
