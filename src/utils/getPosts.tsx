6a97b7a3465bc99ccea56220b9053ecd7a5b364e
LAVINIABENZAR <loredanabenzar2@gmail.com>
2024-06-15 13:31:47 +0300

    refactor: getPosts

--- src/utils/getPosts.tsx
+++ src/utils/getPosts.tsx
@@ -3,21 +3,30 @@ import { supabase } from "./supabase";
 import { Post } from "./postInterface";
 
 
-export const getPosts = async (): Promise<Post[]> => {
+
+export const getPosts = async () => {
+   
     try {
-        const { data: articles, error }: PostgrestResponse<Post> = await supabase
-          .from('articles')
-          .select('*');
-    
-        if (error) {
-          console.error('Error fetching posts:', error);
-          throw new Error(error.message);
-        }
-    
-        return articles || [];
-      } catch (error) {
-        console.error('Unexpected error fetching posts:', error);
-        throw new Error('Failed to fetch posts');
+      const { data, error }: PostgrestResponse<Post> = await supabase
+        .from('articles')
+        .select('*');
+  
+  
+      if (error) {
+        console.error('Error fetching posts:', error);
+        throw new Error(error.message);
       }
-      
-}
+  
+      if (!data) {
+        console.error('No data returned from Supabase');
+        throw new Error('No data returned');
+      }
+  
+
+      console.log('Fetched posts:', data);
+      return data;
+    } catch (error) {
+      console.error('Unexpected error fetching posts:', error);
+      throw new Error('Failed to fetch posts');
+    }
+  };
