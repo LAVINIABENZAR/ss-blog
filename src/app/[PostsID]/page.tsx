67a94fb61658bd2bf2bcf8b8e3adb0bff8a0f2fd
LAVINIABENZAR <loredanabenzar2@gmail.com>
2024-06-17 23:58:22 +0300

    fix: deleted posts file

--- src/app/[PostsID]/page.tsx
+++ src/app/[PostsID]/page.tsx
@@ -1,10 +1,12 @@
 'use client'
 import React from "react";
+import 'tailwindcss/tailwind.css';
 import { getPosts } from "@/utils/getPosts";
 import { useState, useEffect } from "react";
 import { Post } from "@/utils/postInterface";
 
-const Posts = () => {
+const Posts = ({params}: {params: {PostsID: string}}) => {
+   
     const [posts, setPosts] = useState<Post[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
 
@@ -28,7 +30,24 @@ const Posts = () => {
         return <div>Loading...</div>;
       }
     
-    
+       
+      return (
+        <>
+          {posts.map((post) => (
+            <div  key={post.id} className="p-7 bg-slate-400"> 
+            <div>
+              {post.title}
+            </div>
+            <h2>from</h2>
+            <div>
+              {post.movie}
+            </div>
+            <h2>Product ID {post.id}</h2>
+            </div>
+          ))}
+        </>
+      );
+
   };
   
   export default Posts;
