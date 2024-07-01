0133e5a16f55d9848d9dc6bcd5751a7ca7f97506
LAVINIABENZAR <loredanabenzar2@gmail.com>
2024-07-01 21:17:42 +0300

    add: navigation links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+'use client'
+import 'tailwindcss/tailwind.css';
+import React from "react";
+import Link from 'next/link';
+import { Post } from '@/utils/postInterface';
+// import PostsPage from '@/components/postsPage';
+
+interface PostListProps {
+  posts: Post[];
+}
 
 
-export default function Home() {
+export default function Home({posts}: PostListProps) {
   return (
-   <>
-   <div>HELLO WORLD</div>
-   </>
+    <div className=' bg-slate-200'>
+  
+      <Link href='/Editor'><button className=' bg-red-200 p-5'>EDITOR</button></Link>
+      <Link href='/Posts'><button className=' bg-red-200 p-5'>POSTS</button></Link>
+    </div>
   );
 }
