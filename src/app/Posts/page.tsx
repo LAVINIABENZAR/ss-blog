d777b57e629245baa433c22af7840e5c113b8eb8
LAVINIABENZAR <loredanabenzar2@gmail.com>
2024-07-02 22:58:54 +0300

    add deleted page back

---
+++ src/app/Posts/page.tsx
@@ -0,0 +1,53 @@
+'use client'
+import React from "react";
+import 'tailwindcss/tailwind.css';
+import { getPosts } from "@/utils/getPosts";
+import { useState, useEffect } from "react";
+import { Post } from "@/utils/postInterface";
+import Link from "next/link";
+
+
+const Posts = ({params}: {params: {PostsID: string}}) => {
+   
+    const [posts, setPosts] = useState<Post[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+  
+    const fetchPosts = async () => {
+      try {
+        const data = await getPosts();
+        setPosts(data);
+      } catch (error: unknown) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    useEffect(() => {
+        fetchPosts();
+      }, []);
+    
+      if (loading) {
+        return <div>Loading...</div>;
+      }
+    
+  
+      return (
+        <>
+        {posts.map((post) => (
+            <Link href={`/Post/${post.id}`} key={post.id}>
+                <div className="p-7 bg-slate-400">
+                    <div>{post.title}</div>
+                    <h2>from</h2>
+                    <div>{post.movie}</div>
+                    <h2>Product ID {post.id}</h2>
+                </div>
+            </Link>
+        ))}
+    </>
+      );
+
+  };
+  
+  export default Posts;
