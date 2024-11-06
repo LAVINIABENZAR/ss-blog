e9c895bef5bbd732691c3c915b9b53d6e06087c7
LAVINIABENZAR <loredanabenzar2@gmail.com>
2024-11-06 20:18:08 +0200

    posts page

--- src/app/Posts/page.tsx
+++ src/app/Posts/page.tsx
@@ -1,21 +1,15 @@
 'use client'
-import React from "react";
-import type { GetStaticProps } from "next";
+import React, { useEffect, useState } from "react";
 import 'tailwindcss/tailwind.css';
 import { getPosts } from "@/utils/getPosts";
-import { useState, useEffect } from "react";
 import { Post } from "@/utils/postInterface";
 import Link from "next/link";
+ 
 
-
-
-
-const Posts = ({params}: {params: {PostsID: string}}) => {
-   
+const Posts = () => {
     const [posts, setPosts] = useState<Post[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
 
-  const {PostsID} = params
     const fetchPosts = async () => {
       try {
         const data = await getPosts();
@@ -29,37 +23,26 @@ const Posts = ({params}: {params: {PostsID: string}}) => {
 
     useEffect(() => {
         fetchPosts();
-      }, [PostsID]);
+    }, []);
     
-      if (loading) {
-        return <div>Loading...</div>;
-      }
-    
-  
-      return (
-        <>
-        {posts.map((post) => (
-            <Link href={`/Post/${post.id}`} key={post.id}>
-                <div className="p-7 bg-slate-400">
-                    <div>{post.title}</div>
-                    <h2>from</h2>
-                    <div>{post.movie}</div>
-                    <h2>Product ID {post.id}</h2>
-                </div>
-            </Link>
-        ))}
-    </>
-      );
-
-  };
-
-  export async function GetStaticProps() {
-    const posts = await getPosts()
-   return {
-    props: {
-       posts
+    if (loading) {
+      return <div>Loading...</div>;
     }
-   }
-  }
-  
-  export default Posts;
+
+    return (
+      <>
+        {posts.map((post) => (
+          <Link href={`/Posts/${post.id}`} key={post.id}>
+            <div className="p-7 bg-slate-400">
+              <div>{post.title}</div>
+              <h2>from</h2>
+              <div>{post.movie}</div>
+              <h2>Product ID {post.id}</h2>
+            </div></Link>
+            
+        ))}
+      </>
+    );
+};
+
+export default Posts;
