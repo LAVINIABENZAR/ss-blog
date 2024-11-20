272d9970e072b301dcecd63f66de3c5eb043baea
LAVINIABENZAR <loredanabenzar2@gmail.com>
2024-11-20 23:46:50 +0200

    refactor main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,14 @@ import 'tailwindcss/tailwind.css';
 import React from "react";
 import Link from 'next/link';
 import { Post } from '@/utils/postInterface';
-// import PostsPage from '@/components/postsPage';
 
 interface PostListProps {
   posts: Post[];
 }
 
-
 export default function Home({posts}: PostListProps) {
   return (
     <div className=' bg-slate-200'>
-  
       <Link href='/Editor'><button className=' bg-red-200 p-5'>EDITOR</button></Link>
       <Link href='/Posts'><button className=' bg-red-200 p-5'>POSTS</button></Link>
     </div>
